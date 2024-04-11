a640def323b353f17a65d6465a708844c00af728
Socvest <82335390+Socvest@users.noreply.github.com>
2024-04-11 14:20:49 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # streamlit-browser-engine
-Get important statistics from your browser like the user agent, browser type (Chrome, IE, Firefox etc) and more
+
+Streamlit component that allows you to get browser stats like user agent or whether the app is being run on mobile or desktop or if you are using Chrome, Firefox, IE etc and more.
+
+## Installation instructions
+
+```sh
+pip install streamlit-browser-engine
+```
+
+## Usage instructions
+
+```python
+import streamlit as st
+from browser_detection import browser_detection_engine
+
+value = browser_detection_engine()
+st.write(value)
+
+```
+
+params:
+- singleRun [defaults to True] - if you only want component to run when app first runs. It will only run once and get the data once. Use False if you want to run everytime app re-runs. 
---
+++ setup.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import setuptools
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name="streamlit-browser-engine",
+    version="0.0.1",
+    author="",
+    author_email="",
+    description="Streamlit component that allows you to get browser stats like user agent or whether the app is being run on mobile or desktop and more.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.7",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+    ],
+    extras_require={
+        "devel": [
+            "wheel",
+            "pytest==7.4.0",
+            "playwright==1.39.0",
+            "requests==2.31.0",
+            "pytest-playwright-snapshot==1.0",
+            "pytest-rerunfailures==12.0",
+        ]
+    }
+)
