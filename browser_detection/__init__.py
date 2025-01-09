c4063c1540d6b4717e81885264edefafce79bc9a
Socvest <82335390+Socvest@users.noreply.github.com>
2025-01-09 21:21:09 +0000

    Update __init__.py

--- browser_detection/__init__.py
+++ browser_detection/__init__.py
@@ -1,7 +1,8 @@
 import os
+import time 
 import streamlit as st
+from typing import Literal, Optional, Union
 import streamlit.components.v1 as components
-
 
 _RELEASE = True
 
@@ -13,7 +14,7 @@ else:
     _browser_detection_engine = components.declare_component("browser_detection_engine", path=build_dir)
 
 
-def browser_detection_engine(singleRun:bool=False, key:str="browser_engine"):
+def browser_detection_engine(timeout:Union[int, float]=5, interval:Union[int, float]=0.1, key:str="browser_engine"):
 
     """
         Get browser stats like user agent, whether app is running on a desktop, tablet, mobile etc.
@@ -22,7 +23,14 @@ def browser_detection_engine(singleRun:bool=False, key:str="browser_engine"):
             - singleRun (bool): [Defaults to True] Whether to run when component first mounts and only then or if you want it to keep updating when app updates.
     """
 
-    value = _browser_detection_engine(singleRun=singleRun, key=key, default={})
-
+    value = _browser_detection_engine(key=key, default=None)
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        value = st.session_state.get(key)
+        if value is not None:
+            return value
+        time.sleep(interval)
+    
+    st.warning(f"Timeout reached while waiting for window size information. Using default value: {None}")
     return value
 
