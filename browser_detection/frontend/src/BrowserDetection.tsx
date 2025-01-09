ce0f29e5d327da07839e7aa2cd302ab881ff4aae
Socvest <82335390+Socvest@users.noreply.github.com>
2025-01-09 21:20:48 +0000

    Update BrowserDetection.tsx

--- browser_detection/frontend/src/BrowserDetection.tsx
+++ browser_detection/frontend/src/BrowserDetection.tsx
@@ -4,40 +4,26 @@ import {
   withStreamlitConnection,
   ComponentProps
 } from "streamlit-component-lib"
-import React, { ReactNode, useState, useEffect } from "react"
+import React, { ReactNode, useState, useEffect, useMemo } from "react"
 import BrowserDetector from 'browser-dtector'
 
 
 const BrowserDetection: React.FC<ComponentProps> = (props: any) => {
 
   const { args } = props
-  const singleRun: any = args["singleRun"]
-
-  const browser = new BrowserDetector(window.navigator.userAgent);
+  const browser = useMemo(() => new BrowserDetector(window.navigator.userAgent), []); 
 
   useEffect(() => {
     Streamlit.setFrameHeight(0)
   })
 
   useEffect(() => {
-    if (!singleRun) {
-      Streamlit.setComponentValue(browser.parseUserAgent())
-    }
+    Streamlit.setComponentValue(browser.parseUserAgent())
+    Streamlit.setComponentReady()
 
-  })
-  useEffect(() => {
+  }, [browser]) 
 
-    if (singleRun) {
-      Streamlit.setComponentValue(browser.parseUserAgent())
-      Streamlit.setComponentReady()
-    }
-
-  }, [])
-
-  return (
-    <div style={{ display: "none" }}>
-    </div>
-  )
+  return <></>
 
 }
 
